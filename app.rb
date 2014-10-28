b0b997b0966b741d3deb828c83e115a3030fdd01
Greg Narajka <gregnar@gmail.com>
2014-10-28 14:52:14 -0600

    delete implemented, all good

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'bundler'
 Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
   not_found do
     erb :error
   end
@@ -19,6 +21,11 @@ class IdeaBoxApp < Sinatra::Base
     idea = Idea.new(params['idea_title'], params['idea_description'])
     idea.save
     redirect '/'
-    end
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
+  end
 
 end
